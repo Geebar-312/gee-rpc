7cb10c14813ff8872278cbdb0f4a0a4f65d83a7e
Geebar <3399457143@qq.com>
2025-11-29 13:58:36 +0800

    本地服务注册器代码开发

--- example-provider/src/main/java/com/geebar/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/geebar/example/provider/EasyProviderExample.java
@@ -2,6 +2,8 @@ package com.geebar.example.provider;
 
 
 
+import com.geebar.example.common.service.UserService;
+import com.geebar.geerpc.registry.LocalRegistry;
 import com.geebar.geerpc.server.HttpServer;
 import com.geebar.geerpc.server.VertxHttpServer;
 
@@ -11,6 +13,9 @@ import com.geebar.geerpc.server.VertxHttpServer;
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
+
+        //注册服务
+        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
         //启动web服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
